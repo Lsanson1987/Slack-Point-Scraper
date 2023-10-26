2e6075b7c8596dd2182fe7435c7588930287ab77
VectorOverflow <lsanson1987@gmail.com>
2023-10-26 17:43:50 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,36 @@
+from slack import WebClient
+import re
+
+# Initialize the Slack client with your bot's API token
+slack_token = 'xoxb-5857357875557-6115749109201-CoRsy4zN6Nf2mMIFrZMqhXPG'
+client = WebClient(token=slack_token)
+
+# Define a dictionary to store pledge points for each user
+pledge_points = {}
+
+# Channel ID where pledge point changes are reported
+channel_id = 'pledge-points'
+
+# Get messages from the channel
+response = client.conversations_history(channel=channel_id)
+
+# Process each message
+for message in response['messages']:
+    user_id = message['user']
+    text = message['text']
+
+    # Use regular expressions to find pledge point changes
+    changes = re.findall(r'[+-]\d+', text)
+
+    # Update pledge points for the user
+    for change in changes:
+        change_value = int(change)
+        if user_id not in pledge_points:
+            pledge_points[user_id] = 0
+        pledge_points[user_id] += change_value
+
+# Display the total pledge points for each user
+for user_id, total_points in pledge_points.items():
+    user_info = client.users_info(user=user_id)
+    user_name = user_info['user']['real_name']
+    print(f'{user_name}: {total_points} points')
