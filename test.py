31a7269963e2143034754788119c5030de6483ab
VectorOverflow <141704489+Lsanson1987@users.noreply.github.com>
2023-10-30 15:19:19 -0500

    Update test.py

--- test.py
+++ test.py
@@ -10,28 +10,32 @@ client = WebClient(token=slack_token)
 pledge_points = {}
 
 # Channel ID where pledge point changes are reported
-channel_id = 'pledge-points'
+channel_id = 'channel_id'
 
 # Get messages from the channel
-response = client.conversations_history(channel=channel_id)
+response = client.conversations_history(channel=channel_id, limit=270)
+
+# Regular expression to match pledge points at the start of a message
+pattern = re.compile(r'^[+-]?\s*\d+')
 
 # Process each message
 for message in response['messages']:
     user_id = message['user']
     text = message['text']
 
-    # Use regular expressions to find pledge point changes
-    changes = re.findall(r'[+-]?\s*\d+', text)
-
-    # Update pledge points for the user
-    for change in changes:
-        change_value = int(change.replace(" ", "").replace("+", ""))
+    # Check if the message starts with a pledge point change
+    match = pattern.match(text)
+    if match:
+        change_value = int(match.group().replace(" ", "").replace("+", ""))
         if user_id not in pledge_points:
             pledge_points[user_id] = 0
         pledge_points[user_id] += change_value
 
-# Display the total pledge points for each user
-for user_id, total_points in pledge_points.items():
+# Sort users by total pledge points in descending order
+sorted_users = sorted(pledge_points.items(), key=lambda x: x[1], reverse=True)
+
+# Display the sorted list of users and their total pledge points
+for user_id, total_points in sorted_users:
     user_info = client.users_info(user=user_id)
     user_name = user_info['user']['real_name']
     print(f'{user_name}: {total_points} points')
